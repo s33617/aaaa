7db49b10ba0d9654958a99c0c44001915ef05e89
Mikołaj Sienicki <s33617@pjwstk.edu.pl>
2025-03-20 13:38:52 +0100

    zadanienieskonczone

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    public String fname;
+    public String lname;
+    public String indexNumber;
+    public String email;
+    public String adress;
+    public int grades;
+    public Student(String fname, String lname, String indexNumber, String email, String adress, int grades) {
+        this.fname = fname;
+        this.lname = lname;
+        this.indexNumber = indexNumber;
+        this.email = email;
+        this.adress = adress;
+        this.grades = grades;
+    }
+    public double CalculateAverage() {
+
+    }
+
+
+
+}
